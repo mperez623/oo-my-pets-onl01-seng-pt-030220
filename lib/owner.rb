67a1ac4151c2b17361e5a0bb9ff69de6e29e13dd
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-08 05:15:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{self.dogs.count}dog(s), and #{self.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
     
 end
