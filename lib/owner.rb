af0bac37dc8e08a77dd1294c6cc91877e0c76076
Melissa Perez <melissaperezg.96@gmail.com>
2020-04-08 04:21:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,40 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+ 
+ attr_reader :name, :species
+ 
+ def self.all
+   @@all 
+ end
+ 
+ def self.count 
+   @@all.count
+ end
+ 
+ def self.reset_all 
+   @@all.clear
+ end
+ 
+ def initialize(name)
+   @name = name
+   @species = "human"
+   @@all << self
+ end
+ 
+ def say_species
+   "I am a human."
+ end
+    
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+    
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+    
+    
+    
+    
 end
